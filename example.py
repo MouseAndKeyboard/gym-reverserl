82e33937f72c8dfa1efc63b02d02e519672e6387
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-06-14 16:23:06 +0800

    Unified the way the expert samples the environment

--- example.py
+++ example.py
@@ -11,7 +11,8 @@ expertEnvPolicy = gym.make("MountainCar-v0")
 # we want to create some buffer of environment_state, agent_action pairs 
 # expert env takes the previous state and considers the agent's action to "decide"
 # what the next environemnt state should be.
-samples = []
+expert_observations = []
+expert_actions = []
 for episode in range(1):
     previous_environment_state = expertEnvPolicy.reset()
     for step in range(1):
@@ -20,9 +21,15 @@ for episode in range(1):
         
         # reward and done could possibly be part of the environment_state
         environment_state, reward, done, info = expertEnvPolicy.step(agent_action)
-        samples.append(((previous_environment_state, agent_action), environment_state)) # observation, action
-        
+
+        expert_observations.append((previous_environment_state), agent_action)
+        expert_actions.append(environment_state)
+       
         previous_environment_state = environment_state
+
+
+
+# Now we can do "behavioural cloning" on the expert. This will amount to predicting the next state given a previous state+action
 
 
 def randomAgentPolicy(environment_state, env):
